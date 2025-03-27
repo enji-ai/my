d2369c286c8a8e2cd615cc160460a7e302995f7e
myenji <enjisecond559@gmail.com>
2025-03-27 23:15:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -40,31 +40,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
  
-      let text = `> Hello,  
-> I am enang or usually called enji. 
-> I am a student and also a santri at the Jabal Toriq Islamic boarding school. 
-> My favorite language is javascript programming and anime. 
-> Hehe, I am not a wibu. 
-> I am 18 years old. My date of birth is 03-22-2007`;
-    
-    let index = 0;
-    let speed = 50; // Kecepatan mengetik (ms)
-    let terminalText = document.getElementById("terminal-text");
-    let cursor = document.querySelector(".cursor");
-
-    function typeEffect() {
-        if (index < text.length) {
-            terminalText.innerHTML += text.charAt(index);
-            index++;
-            setTimeout(typeEffect, speed);
-        } else {
-            cursor.style.display = "none"; // Hilangkan cursor setelah selesai mengetik
-        }
-    }
-
-    typeEffect();
-      
-    updateProgress(); // Mulai progress bar
  // Dark Mode Toggle
     const darkModeToggle = document.querySelector(".dark-mode-toggle");
     const darkModeIcon = document.getElementById("darkModeIcon");
